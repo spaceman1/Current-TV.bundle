71a24a03005c38ac553e6eff56675d963908ec44
ryan <ryan@Mac-Pro.local>
2011-03-31 19:03:01 -0400

    Removed dead sections

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -23,7 +23,7 @@ def VideoDirectory(sender, url):
 def MainMenu():
 	dir = MediaContainer()
 	dir.Append(Function(DirectoryItem(VideoDirectory,	title="Best Of Current TV", thumb=R("icon-default.png")), url="http://current.com/currenttv.htm"))
-	dir.Append(Function(DirectoryItem(VideoDirectory, title="Currency", thumb=R("currency.png")), url="http://current.com/topics/76253832/currency/default/0.htm"))
+#	dir.Append(Function(DirectoryItem(VideoDirectory, title="Currency", thumb=R("currency.png")), url="http://current.com/topics/76253832/currency/default/0.htm"))
 	dir.Append(Function(DirectoryItem(VideoDirectory, title="Current Edge", thumb=R("currentedge.png")), url="http://current.com/topics/76254312/current_edge/default/0.htm"))
 	dir.Append(Function(DirectoryItem(VideoDirectory, title="Current Fix", thumb=R("currentfix.png")), url="http://current.com/topics/76772252/current_fix/default/0.htm"))
 	dir.Append(Function(DirectoryItem(VideoDirectory, title="Current Ride", thumb=R("currentride.png")), url="http://current.com/topics/76253932/current_ride/default/0.htm"))
@@ -33,8 +33,8 @@ def MainMenu():
 	dir.Append(Function(DirectoryItem(VideoDirectory, title="Daily Fix", thumb=R("dailyfix.png")), url="http://current.com/topics/76254722/daily_fix/default/0.htm"))
 	dir.Append(Function(DirectoryItem(VideoDirectory, title="Infomania", thumb=R("infomania.png")), url="http://current.com/topics/76254712/infomania/default/0.htm"))
 	dir.Append(Function(DirectoryItem(VideoDirectory, title="Joe Central", thumb=R("joe.png")), url="http://current.com/topics/86547961/joe_central/default/0.htm"))
-	dir.Append(Function(DirectoryItem(VideoDirectory, title="Salon", thumb=R("salon.png")), url="http://current.com/topics/77314621/salon/default/0.htm"))
-	dir.Append(Function(DirectoryItem(VideoDirectory, title="Supernews", thumb=R("supernews.png")), url="http://current.com/topics/76254232/supernews/default/0.htm"))
+#	dir.Append(Function(DirectoryItem(VideoDirectory, title="Salon", thumb=R("salon.png")), url="http://current.com/topics/77314621/salon/default/0.htm"))
+#	dir.Append(Function(DirectoryItem(VideoDirectory, title="Supernews", thumb=R("supernews.png")), url="http://current.com/topics/76254232/supernews/default/0.htm"))
 	dir.Append(Function(DirectoryItem(VideoDirectory, title="Vanguard Journalism", thumb=R("vanguard.png")), url="http://current.com/topics/501/vanguard_journalism/default/0.htm"))
 	dir.Append(Function(DirectoryItem(VideoDirectory, title="Viewer Uploads", thumb=R("uploads.png")), url="http://current.com/uploads.htm"))
 	return dir
